5b73b0bb4b3596f4d4973333631f051b47c24248
htdung1305 <htdung13052002@gmail.com>
2023-08-07 00:20:44 +0700

    Home Page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -16,78 +16,58 @@ const HomePage = ({isLoggedIn}) => {
         });
     }, []);
     return (
-        // <div>
+        <div>
         
-        // {isLoggedIn ? (
-        //     <div className="container page">
-        //         <div className="row">
-        //             <div className="col-md-9">
-        //                 <div className="feed">
-        //                     <a className='yourfeed' href="#yourfeed">Your Feed</a>
-        //                     <a className='globalfeed' href="#globalfeed">Global Feed</a>
-        //                 </div>
-        //             </div>
-        //             <div className="col-md-3">
-        //                 <div className="sidebar">
-        //                     Popular Tags
-        //                     <ul>
-        //                         {tags.map(tag => (
-        //                             <li key={tag}>{tag}</li>
-        //                         ))}
-        //                     </ul>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-
-        // ) : (
-        //     <div>
-        //     <div className="container text-center text-white logo">
-        //             <h1>conduit</h1>
-        //             <p>A place to share your knowledge.</p>
-        //     </div>
-        //     <div className='container page'>
-        //         <div className="row">
-        //             <div className="col-md-9">
-        //                 <div className="feed">
-        //                     <a className='globalfeed' href="#globalfeed">Global Feed</a>
-        //                 </div>
-        //             </div>
-        //             <div className="col-md-3">
-        //                 <div className="sidebar border border-0">
-        //                     Popular Tags
-        //                     <ul>
-        //                         {tags.map(tag => (
-        //                             <li key={tag}>{tag}</li>
-        //                         ))}
-        //                     </ul>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        //     </div>
-        // )}
-        // </div>
-                <div className="container page">
-                    <div className="row">
-                        <div className="col-md-9">
-                            <div className='feed'>
+        {isLoggedIn ? (
+            <div className="container page">
+                <div className="row">
+                    <div className="col-md-9">
+                        <div className="feed">
                             <a className='yourfeed' href="#yourfeed">Your Feed</a>
                             <a className='globalfeed' href="#globalfeed">Global Feed</a>
-                            </div>
                         </div>
-                        <div className="col-md-3">
-                            <div className="sidebar">
-                                Popular Tags
-                                <ul>
-                                    {tags.map(tag => (
-                                        <li key={tag}>{tag}</li>
-                                    ))}
-                                </ul>
-                            </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="sidebar">
+                            Popular Tags
+                            <ul>
+                                {tags.map(tag => (
+                                    <li key={tag}>{tag}</li>
+                                ))}
+                            </ul>
                         </div>
                     </div>
                 </div>
+            </div>
+
+        ) : (
+            <div>
+            <div className="container text-center text-white logo">
+                    <h1>conduit</h1>
+                    <p>A place to share your knowledge.</p>
+            </div>
+            <div className='container page'>
+                <div className="row">
+                    <div className="col-md-9">
+                        <div className="feed">
+                            <a className='globalfeed' href="#globalfeed">Global Feed</a>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="sidebar border border-0">
+                            Popular Tags
+                            <ul>
+                                {tags.map(tag => (
+                                    <li key={tag}>{tag}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        )}
+        </div>
     );
 };
 
