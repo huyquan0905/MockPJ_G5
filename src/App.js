385a40225e1e5cdf837a7c3b60d64c594d76b78a
QuanHH <huyquanwork95@gmail.com>
2023-08-08 15:07:05 +0700

    QuanHH commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import HomePage from "./components/HomePage";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import HomePage from "./components/HomePage";
+import PostArticle from "./components/PostArticle";
 
 function App() {
   return (
     <div>
       <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/post" element={<PostArticle />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/register" element={<SignUp />} />
+        </Routes>
+      </Router>
       <Footer />
-      <HomePage />
     </div>
+
   );
 }
 
