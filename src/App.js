c4dd626d5e4db6e900bdb6ec6554489c477a6138
QuanHH <huyquanwork95@gmail.com>
2023-08-19 08:03:24 +0700

    Commit prevent access another page

--- src/App.js
+++ src/App.js
@@ -10,32 +10,22 @@ import Setting from "./components/Setting";
 import ArticleDetail from "./components/ArticleDetail";
 import { Provider } from "react-redux";
 import store from "./components/redux/store";
-import ProtectedRoutes from "./components/redux/ProtectedRoutes";
-import Profile from "./components/Profile"
 
 function App() {
-  const isAuth = localStorage.getItem("token");
   return (
     <div>
       <Header />
       <Provider store={store}>
         <Routes>
-          <Route>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/setting" element={<Setting />} />
-            <Route path="/article-detail" element={<ArticleDetail />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/post" element={<PostArticle />} />
-
-          </Route>
-          <Route element={<ProtectedRoutes auth={isAuth} />}>
-            {/* <Route path="/post" element={<PostArticle />} /> */}
-          </Route>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/post" element={<PostArticle />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/article-detail" element={<ArticleDetail />} />
         </Routes>
       </Provider>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/PostArticle.jsx
+++ src/components/PostArticle.jsx
@@ -1,68 +1,18 @@
-import React from 'react';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import React, {useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 import '../components/style/PostArticle.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const PostArticle = () => {
-    const [article, setArticle] = useState({
-        title: '',
-        description: '',
-        body: '',
-        tagList: ''
-    });
+    const navigate = useNavigate();
+    const accesstoken = (localStorage.getItem("token"));
+    console.log(accesstoken);
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setArticle({ ...article, [name]: value });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const token = localStorage.getItem("token");
-        console.log(token);
-        // axios.post('https://api.realworld.io/api/articles', {
-        //     article: {
-        //         title: article.title,
-        //         description: article.description,
-        //         body: article.body,
-        //         tagList: article.tagList
-        //     }
-        // },
-        //     {
-        //         headers: {
-        //             Authorization: `Bearer ${token}`
-        //         }
-        //     })
-        //     .then(response => {
-        //         console.log(response.data);
-        //     })
-        //     .catch(error => {
-        //         console.error('Lỗi cmn luôn:', error);
-        //     });
-
-
-        axios.post("https://api.realworld.io/api/articles",
-            JSON.stringify({
-                article: {
-                    title: article.title,
-                    description: article.description,
-                    body: article.body,
-                    tagList: article.tagList,
-                },
-            }),
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-            }
-        )
-            .then((res) => {
-                console.log(res);
-            });
-    }
+    useEffect(() => {
+        if(!accesstoken){
+            navigate('/')
+        }
+    }, [accesstoken, navigate])
 
     return (
         <div>
@@ -72,22 +22,15 @@ const PostArticle = () => {
                         <div className="container page">
                             <div className="row">
                                 <div className="col-md-10 offset-md-1 col-xs-12">
-                                    <form onSubmit={handleSubmit}>
+                                    <form>
                                         <fieldset>
-                                            <fieldset className="form-group">
-                                                <input type="text" className="form-control form-control-lg" placeholder="Article Title" name="title" value={article.title} onChange={handleInputChange} />
-                                            </fieldset>
+                                            <fieldset className="form-group"><input type="text" className="form-control form-control-lg" placeholder="Article Title" /></fieldset>
 
-                                            <fieldset className="form-group">
-                                                <input type="text" className="form-control" placeholder="What's this article about?" name="description" value={article.description} onChange={handleInputChange} />
-                                            </fieldset>
+                                            <fieldset className="form-group"><input type="text" className="form-control" placeholder="What's this article about?" /></fieldset>
 
-                                            <fieldset className="form-group">
-                                                <textarea className="form-control" style={{ marginBottom: "20px" }} rows="8" placeholder="Write your article (in markdown)" name="body" value={article.body} onChange={handleInputChange}></textarea>
-                                            </fieldset>
+                                            <fieldset className="form-group"><textarea className="form-control" style={{ marginBottom: "20px" }} rows="8" placeholder="Write your article (in markdown)" ></textarea></fieldset>
 
-                                            <fieldset className="form-group">
-                                                <input type="text" className="form-control" placeholder="Enter tags" name="tagList" value={article.tagList} onChange={handleInputChange} />
+                                            <fieldset className="form-group"><input type="text" className="form-control" placeholder="Enter tags" />
                                                 <div className="tag-list"></div>
                                             </fieldset>
                                             <button className="btn btn-lg pull-xs-right btn-primary" type="submit">Publish Article</button>
